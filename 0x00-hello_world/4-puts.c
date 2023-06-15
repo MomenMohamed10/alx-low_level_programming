a04a630d63a5d85521a5c3ef0b4c39b80f8ab3f2
MomenMohamed <8lbozchannel@gmail.com>
2023-06-15 13:26:48 -0700

    momen

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
  * main - is the main
  *
  * Decription: practise in alx
- *
- * Return: 0always 
+ * 
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
