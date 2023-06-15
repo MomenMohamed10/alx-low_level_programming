2ef2a590b455f621581ad322a5ebabbe2c38f457
MomenMohamed <8lbozchannel@gmail.com>
2023-06-15 14:19:42 -0700

    momen

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - main
+ * Discription: second task
+ * Return: 0 always
+ *
+*/
+
+{
+int main(void)
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
