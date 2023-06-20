cec94e0139c4d580742b3ac8d632e510a8323556
MomenMohamed <8lbozchannel@gmail.com>
2023-06-20 11:46:06 -0700

    asd

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 or another
+ *
+*/
+void firsttest()
+{
+	printf("_putchar\n");
+}
+
+	int main()
+{
+
+	firsttest();
+	return (0);
+}
