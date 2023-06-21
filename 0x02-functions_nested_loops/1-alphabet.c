e31fa59d0aa1d760962a9b2bce0660f4d16c1b44
MomenMohamed <8lbozchannel@gmail.com>
2023-06-21 10:37:52 -0700

    lk

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * print_alphabet - second task
+ *
+ * Return: 0 always
+*/
+        void print_alphabet(void)
+{
+        char a;
+        for (a = 'a'; a <= 'z'; a++)
+       
+        _putchar(a);
+        _putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - entry point 
+ * print_alphabet_x10: ya rab tnga7
+ *
+*/
+
+void print_alphabet_x10(void)
+{
+	int line, ch;
+
+	for (line = 0; line <= 9; line++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
