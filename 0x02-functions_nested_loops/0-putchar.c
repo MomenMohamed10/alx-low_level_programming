bb818fad3ab7b98247d9df605ba3a75e90d874e7
MomenMohamed <8lbozchannel@gmail.com>
2023-06-21 17:24:17 -0700

    iuy

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,12 @@
-#include "main.h"
-	/**
-	 * main - print _putchar
-	 * is written by mbah
-	 * Return: 0
-	 */
-
-
-	int main(void)
-	{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-	}
+#include <stdio.h>
+/**
+ * main - enrty point
+ *
+ * Return: 0 always
+ *
+*/
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
